82bcadf8df727f74f380e6b42db4779c3e80822f
Fernando Rodrigues <fernando_tecti@hotmail.com>
2022-12-08 23:45:59 -0300

    add more links

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,20 +1,47 @@
 let tableItems = [
-    {
-       nome: "VSCODE",
-       categoria: "Editor de código online",
-       descricao: "O Visual Studio Code é um editor de código desenvolvido pela microsoft...",
-       site: "https://vscode.dev/"
-    },
-    {
-       nome: "FRONTEDITOR",
-       categoria: "Editor de código online",
-       descricao: "Nenhuma descrição disponível...",
-       site: "https://fronteditor.dev/"
-    },
-    {
-       nome: "CODEPEN",
-       categoria: "Editor de código online",
-       descricao: "O Codepen é um editor de código online desenvolvido pela...",
-       site: "https://codepen.io/pen/"
-    }
- ];
+   {
+      nome: "VSCODE",
+      categoria: "Editor de código online",
+      descricao: "Versão online do visual studio code da microsoft",
+      site: "https://vscode.dev/"
+   },
+   {
+      nome: "fronteditor",
+      categoria: "Editor de código online",
+      descricao: "Um bom editor de código online com comportamento igual ao vscode",
+      site: "https://fronteditor.dev/"
+   },
+   {
+      nome: "codepen",
+      categoria: "Editor de código online",
+      descricao: "O Codepen é um editor de código online desenvolvido pela...",
+      site: "https://codepen.io/pen/"
+   },
+   {
+      nome: "readme.so",
+      categoria: "Editor de código online",
+      descricao: "Com o readme.so é possível gerar um readme.md para o github de forma fácil",
+      site: "https://readme.so/pt/editor/"
+   },
+   {
+      nome: "icomoon.io",
+      categoria: "SVG/Imagens",
+      descricao: "Com o icomooon você pode importar o css para utilizar os svgs disponíveis na página",
+      site: "https://icomoon.io/app/#/select"
+   },
+   {
+      nome:       "Badges para GITHUB",
+      categoria:  "Repositórios",
+      descricao:  "Melhor repositório de badges para utilizar no profile do github, só copiar e colar",
+      site:       "https://github.com/iuricode/readme-template/blob/main/badges/badges.md"
+   },
+];
+
+let example = [
+   {
+      nome:       "",
+      categoria:  "",
+      descricao:  "",
+      site:       ""
+   },
+];
