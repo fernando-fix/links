8659dd447f8eaaa657030c681b9f88ec1051cd0c
Fernando Rodrigues <fernando_tecti@hotmail.com>
2022-12-09 23:08:49 -0300

    more links added

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,39 +1,51 @@
 let tableItems = [
    {
-      nome: "VSCODE",
-      categoria: "Editor de código online",
-      descricao: "Versão online do visual studio code da microsoft",
-      site: "https://vscode.dev/"
+      nome:       "vscode",
+      categoria:  "Editor de código online",
+      descricao:  "Versão online do visual studio code da microsoft",
+      site:       "https://vscode.dev/"
    },
    {
-      nome: "fronteditor",
-      categoria: "Editor de código online",
-      descricao: "Um bom editor de código online com comportamento igual ao vscode",
-      site: "https://fronteditor.dev/"
+      nome:       "fronteditor",
+      categoria:  "Editor de código online",
+      descricao:  "Um bom editor de código online com comportamento igual ao vscode",
+      site:       "https://fronteditor.dev/"
    },
    {
-      nome: "codepen",
-      categoria: "Editor de código online",
-      descricao: "O Codepen é um editor de código online desenvolvido pela...",
-      site: "https://codepen.io/pen/"
+      nome:       "codepen",
+      categoria:  "Editor de código online",
+      descricao:  "O Codepen é um editor de código online desenvolvido pela...",
+      site:       "https://codepen.io/pen/"
    },
    {
-      nome: "readme.so",
-      categoria: "Editor de código online",
-      descricao: "Com o readme.so é possível gerar um readme.md para o github de forma fácil",
-      site: "https://readme.so/pt/editor/"
+      nome:       "readme.so",
+      categoria:  "Editor de código online",
+      descricao:  "Com o readme.so é possível gerar um readme.md para o github de forma fácil",
+      site:       "https://readme.so/pt/editor/"
    },
    {
-      nome: "icomoon.io",
-      categoria: "SVG/Imagens",
-      descricao: "Com o icomooon você pode importar o css para utilizar os svgs disponíveis na página",
-      site: "https://icomoon.io/app/#/select"
+   nome:          "icomoon.io",
+      categoria:  "SVG/Imagens",
+      descricao:  "Com o icomooon você pode importar o css para utilizar os svgs disponíveis na página",
+      site:       "https://icomoon.io/app/#/select"
    },
    {
-      nome:       "Badges para GITHUB",
+      nome:       "badges para GITHUB",
       categoria:  "Repositórios",
       descricao:  "Melhor repositório de badges para utilizar no profile do github, só copiar e colar",
       site:       "https://github.com/iuricode/readme-template/blob/main/badges/badges.md"
+   },
+   {
+      nome:       "image extractor",
+      categoria:  "SVG/Imagens",
+      descricao:  "Extrai imagens e svg do site que você quiser",
+      site:       "https://extract.pics/"
+   },
+   {
+      nome:       "cdnjs",
+      categoria:  "Import data",
+      descricao:  "Site que contém diversos links cdn para importar, Ex.: fonte awesome, bootstrap e etc...",
+      site:       "https://cdnjs.com/"
    },
 ];
 
