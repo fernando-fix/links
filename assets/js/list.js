846d69350accb451647504150d1f6b36f526bae2
Fernando Rodrigues <fernando_tecti@hotmail.com>
2022-12-17 17:47:53 -0300

    add vários sites

--- assets/js/list.js
+++ assets/js/list.js
@@ -71,6 +71,54 @@ let tableItems = [
       descricao:  "links para auxiliar no desenvolvimento frontend",
       site:       "https://frontpush.notion.site/frontpush/Notion-FrontPUSH-2-0-ff857f2b78234a6981d1e4d40874a6cc"
    },
+   {
+      nome:       "free-css",
+      categoria:  "Templates",
+      descricao:  "Site com templates gratuítos de páginas para fazer download",
+      site:       "https://www.free-css.com/free-css-templates"
+   },
+   {
+      nome:       "Edrawmind",
+      categoria:  "Mapa mental",
+      descricao:  "Site para fazer mapas mentais",
+      site:       "https://www.edrawmind.com/online/create?lang=pt"
+   },
+   {
+      nome:       "ProgramaThor",
+      categoria:  "Vagas para DEVS",
+      descricao:  "Site para encontrar vagas de emprego para desenvolvedores",
+      site:       "https://programathor.com.br/"
+   },
+   {
+      nome:       "Picsum",
+      categoria:  "Gerador de fotos",
+      descricao:  "Site para gerar uma imagem aleatória para seu projeto, o seguinte endereço: https://picsum.photos/1920/1080 vai gerar uma foto de 1920X1080",
+      site:       "https://picsum.photos/"
+   },
+   {
+      nome:       "Simpleicons",
+      categoria:  "SVG/Imagens",
+      descricao:  "Site com vários SVGS, é só clicar em cima da imagem para copiar o código SVG e colar no seu projeto",
+      site:       "https://simpleicons.org/"
+   },
+   {
+      nome:       "Fakenamegenerator",
+      categoria:  "Gerador de nomes",
+      descricao:  "Site para gerar nomes aleatórios para testar os cadastros no seu projeto",
+      site:       "https://pt.fakenamegenerator.com/gen-random-br-br.php"
+   },
+   {
+      nome:       "Freewha",
+      categoria:  "Hospedagem Free",
+      descricao:  "Hospedagem PHP/Mysql de graça para testar/hospedar seu projeto",
+      site:       "https://freewha.com/"
+   },
+   {
+      nome:       "Shields",
+      categoria:  "Badges",
+      descricao:  "Site para gerar badges personalizadas, muito bom para colocar no perfil do github",
+      site:       "https://shields.io/"
+   },
 ];
 
 let example = [
